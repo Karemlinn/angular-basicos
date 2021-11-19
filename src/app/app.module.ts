c218a0ac47d1f621d64e6a98e6f6f4f67b8b918d
Karem <karemlinn@gmail.com>
2021-11-19 14:32:46 +0100

    mi primer comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
 
+//Tarea , crear un modulo. contador.modulo
+//declaraciones y exportaciones
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
